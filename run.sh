f8f5a585f20b6f216f0cf5e409647426f3bf0b05
VOIDMalkuth <8085528+VOIDMalkuth@users.noreply.github.com>
2022-04-15 14:20:47 +0800

    fix run.sh error

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ mkdir -p $RUNLOG_ROOT
 # load environments
 echo "Setting environments ..."
 source scripts/env.sh
-running motivation
+# running motivation
 echo "-------- Evaluating motivation -------"
 echo "Log file: $RUNLOG_ROOT/motivation.log"
 cd DynVec-motivation
@@ -24,7 +24,7 @@ sh scripts/run_tools/get_data.sh > $RUNLOG_ROOT/get_data.log 2>&1 && \
 SPMV_DATA=`pwd`/data/mx_data_less_than_1G/
 SPMV_LOG_ROOT=$RUNLOG_ROOT/spmv
 mkdir -p $SPMV_LOG_ROOT
-running SpMV evaluations
+# running SpMV evaluations
 echo "--------- Evaluating SpMV ---------"
 echo "Evaluating DynVec..."
 { set -e; sh scripts/run_tools/spmv/run_dynvec.sh $SPMV_DATA $SPMV_LOG_ROOT; } > $SPMV_LOG_ROOT/run.log 2>&1 && \
